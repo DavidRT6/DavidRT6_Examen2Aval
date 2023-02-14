bb6c256e1b956ef8b3421571b4ef3b9580dce5f9
David Roca <darota@floridauniversitaria.es>
2023-02-14 15:22:19 +0100

    Acabado atotal.sh

---
+++ atotal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cantConsumos=`cat consumos.txt | tail -n+2 | wc -l`
+total=0
+
+read -p "Introduce una ciudad: " ciudadElegida
+
+while [ $total -eq 0 ]; do
+for i in `seq 1 $cantConsumos`; do
+ciudad=`cat consumos.txt | awk '{print $1}' | head -$i | tail -1`
+consumo=`cat consumos.txt | awk '{print $4}' | head -$i | tail -1`
+if [ $ciudad = $ciudadElegida ]
+then
+total=$((total+consumo))
+fi
+done
+
+if [ $total -eq 0 ]
+then
+echo "Esa ciudad no se encuentra en la lista."
+read -p "Introduce otra: " ciudadElegida
+fi
+done
+
+echo
+echo "El consumo de la ciudad $ciudad es de: $total"
